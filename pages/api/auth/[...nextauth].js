aada4924ad1962e051c19cf50fc1c8d14bd0b681
Gatlin <wwgdb18@gmail.com>
2024-03-29 17:07:20 -0500

    update to make it work on vercel #14

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,12 +7,12 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 
   adapter: FirestoreAdapter(db),
-  secret: process.env.NEXTAUTH_SECRET,
 });
