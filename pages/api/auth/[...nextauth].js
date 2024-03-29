60c1547725ba6db2c7aa734dadaad9fc036772b9
Gatlin <wwgdb18@gmail.com>
2024-03-29 16:39:27 -0500

    update to make it work on vercel #13

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 
   adapter: FirestoreAdapter(db),
 });
